0ea3c933b80d7ea480dcd2dda88d8d806d61e1b7
Bradley Matusiak <bmatusiak@gmail.com>
2020-07-27 14:45:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 # install nvm node npm
 # install nvm
 bash ./setup-scripts/install-nvm-node-npm.sh
+
+npm install
+
 sudo su - -c "bash /home/pi/YAPP/setup-scripts/install-nvm-node-npm.sh"
 
 bash ./setup-scripts/install-pm2.sh
@@ -10,8 +13,5 @@ sudo su - -c "bash /home/pi/YAPP/setup-scripts/install-pm2.sh"
 
 
 bash ./setup-scripts/install-c9sdk.sh
-
-cd /home/pi/YAPP
-npm install
 
 sudo su - -c "bash /home/pi/YAPP/setup-scripts/install-root-pm2-services.sh"
