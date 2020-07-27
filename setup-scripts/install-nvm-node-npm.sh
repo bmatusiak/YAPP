9521be38dab1c91178fe7cf1b58c3633187e99e5
Bradley Matusiak <bmatusiak@gmail.com>
2020-07-27 13:16:27 -0400

    Update install-nvm-node-npm.sh

--- setup-scripts/install-nvm-node-npm.sh
+++ setup-scripts/install-nvm-node-npm.sh
@@ -3,7 +3,14 @@ wget https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh -O nvm-inst
 sudo bash nvm-install.sh
 bash nvm-install.sh
 
-sudo nvm i v10
-nvm i v10
+source .nvm/nvm.sh
+
+sudo su - -c 'nvm i v10.21'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm i v10.21
 
 
