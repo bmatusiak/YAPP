b328f500280c40337003d5c9236591ca8e6cdbba
Bradley Matusiak <bmatusiak@gmail.com>
2020-07-27 13:40:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,9 @@
 # install nvm
 bash ./setup-scripts/install-nvm-node-npm.sh
 sudo su - -c "bash /home/pi/YAPP/setup-scripts/install-nvm-node-npm.sh"
+
+bash ./setup-scripts/install-pm2.sh
+sudo su - -c "bash /home/pi/YAPP/setup-scripts/install-pm2.sh"
+
+
 bash ./setup-scripts/install-c9sdk.sh
