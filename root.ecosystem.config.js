3105a79b18cc0b00b1318aff3cec67aac03d5961
Bradley Matusiak <bmatusiak@gmail.com>
2020-07-27 14:41:39 -0400

    Update root.ecosystem.config.js

--- root.ecosystem.config.js
+++ root.ecosystem.config.js
@@ -2,6 +2,20 @@ module.exports = {
   
   apps : [{
     name: "DHCP-server",
+    cwd: '/home/pi/YAPP/DHCP-server',
+    script: 'index.js'
+  },{
+    name: "DNS-server",
+    cwd: '/home/pi/YAPP/DNS-server',
+    script: 'index.js'
+  },{
+    name: "HID-setup",
+    cwd: '/home/pi/YAPP/HID-server',
+    script: 'index.js',
+    autorestart : false
+  },{
+    name: "HID-server-proxy",
+    cwd: '/home/pi/YAPP/HID-server-proxy',
     script: 'index.js'
   }]
   
