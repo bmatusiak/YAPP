ed781cba7558e225c86456abe9fb881cea3807de
Bradley Matusiak <bmatusiak@gmail.com>
2020-07-27 13:45:42 -0400

    Update install-system-deps.sh

--- setup-scripts/install-system-deps.sh
+++ setup-scripts/install-system-deps.sh
@@ -1,1 +1,1 @@
-sudo apt-get install build-essential libusb-1.0-* libudev* nginx nginx-extras git -y
+sudo apt-get install build-essential libusb-1.0-* libudev* nginx nginx-extras git tmux libncurses6 -y
