def99f1a5d773d0cdffecb4ad3fe4217162e9e20
Bradley Matusiak <bmatusiak@gmail.com>
2020-07-27 13:01:27 -0400

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,7 +1,11 @@
 
 wget -qO- https://raw.githubusercontent.com/bmatusiak/YAPP/master/setup-scripts/install-system-deps.sh | bash
 
-git clone https://github.com/bmatusiak/YAPP.git
+cd /home/pi
+
+rm -rf ./YAPP
+
+git clone https://github.com/bmatusiak/YAPP.git YAPP
 
 cd YAPP
 
